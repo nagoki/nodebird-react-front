5849b2a0d3df4be42ce4454fee354f1a41c15a8a
leejh3 <jhlee971016@gmail.com>
2022-09-26 11:51:10 +0900

    first commit

---
+++ componets/AppLayout.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { Input, Menu, Row, Col } from 'antd';
+import styled from 'styled-components';
+import UserProfile from '../componets/UserProfile';
+import LoginForm from '../componets/LoginForm';
+
+const SearchInput = styled(Input.Search)`
+    vertical-align: middle;
+`;
+
+const AppLayout = ({ children }) => {
+    const [isLoggedIn, setLoggedIn] = useState(false);
+
+    return (
+        <div>
+            <Menu mode="horizontal">
+                <Menu.Item>
+                    <Link href="/"><a>노드버드</a></Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href="/profile"><a>프로필</a></Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Input.Search enterButton />
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href="/signup"><a>회원가입</a></Link>
+                </Menu.Item>
+            </Menu>
+            <Row gutter={8}>
+                <Col xs={24} md={6}>
+                    {isLoggedIn ? <UserProfile /> : <LoginForm />}
+                </Col>
+                <Col xs={24} md={12}>
+                    {children}
+                </Col>
+                <Col xs={24} md={6}>
+                    <a href="https://devdep.tistory.com/" target="_blank" rel="noreferrer noopener">Made by Nagoki</a>
+                </Col>
+            </Row>
+        </div>
+    )
+};
+
+AppLayout.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default AppLayout;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppLayout from "../componets/AppLayout";
+
+const Home = () => {
+    return (
+        <AppLayout>
+            <div>
+                Hello, Next!
+            </div>
+        </AppLayout>
+    );
+}
+
+export default Home;
